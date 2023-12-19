23e4c8348bb0183bdbe3eefa79a68647f0b81594
Nikhil Devadiga <nmdevadiga216@gmail.com>
2023-12-19 12:49:39 +0530

    Added ast

--- src/demo_lang/compile.py
+++ src/demo_lang/compile.py
@@ -11,53 +11,58 @@ def parse(source):
         | var_statement
         | obj_statement
         | constr_statement
-    var_statement: 'var' var_type iden '=' var_expr
+    var_statement: 'var' var_type lhs=iden '=' rhs=var_expr { ('VAR', var_type, [lhs, rhs])}
     var_type:
-        | 'cont'
-        | 'int'
-        | 'bin'
-    var_expr: 'ndarray' '(' ','.base_expr+ ')'
-    constr_statement: 'constr' expr
-    obj_statement: 'obj' obj_func expr
+        | 'cont'                                            { 'CONT' }
+        | 'int'                                             { 'INT' }
+        | 'bin'                                             { 'BIN' }
+    var_expr: 'ndarray' '(' ','.base_expr+ ')'              { 'FUNC', 'NDARRAY', []}
+    constr_statement: 'constr' expr                         { 'CONSTR', None, [expr]}
+    obj_statement: 'obj' obj_func expr                      { ('OBJ', obj_func, [expr])}
     obj_func:
-        | 'min'
-        | 'max'
+        | 'min'                                             { 'MIN' }
+        | 'max'                                             { 'MAX' }
     expr:
         | func_expr
         | comp_op_expr
-    func_expr: func_iden ('(' iter_expr (','comp_op_expr)* ')')+ expr
+    func_expr: func=func_iden b=block+ expr                 { (*func, [expr, *b])}
+    block: '(' it=iter_expr re=rest* ')'                    { ('BLOCK', None, [it, *re]) }
+    rest: ',' comp_op_expr                                  { comp_op_expr }
     func_iden:
-        | 'sum'
-        | 'forall'
-    iter_expr: iden ':' set_expr
+        | 'sum'                                             { ('FUNC', 'SUM')}
+        | 'forall'                                          { ('FUNC', 'FORALL')}
+    iter_expr: lhs=iden ':' rhs=set_expr                    { ('OP', 'ITER', [lhs, rhs])}
     set_expr:
         | iden
     comp_op_expr:
-        | add_sub_op_expr comp_op add_sub_op_expr 
+        | lhs=add_sub_op_expr op=comp_op rhs=add_sub_op_expr { (*op, [lhs, rhs])}
         | add_sub_op_expr
     comp_op:
-        | '!='
-        | '=='
-        | '<='
-        | '>='
+        | '!='                                              { ('OP', 'NE')}
+        | '=='                                              { ('OP', 'EQ')}
+        | '<='                                              { ('OP', 'LE')}
+        | '>='                                              { ('OP', 'GE')}
+        | '<'                                               { ('OP', 'LT')}
+        | '>'                                               { ('OP', 'GT')}
     add_sub_op_expr:
-        | mul_div_op_expr '+' add_sub_op_expr
-        | mul_div_op_expr '-' add_sub_op_expr
+        | lhs=mul_div_op_expr '+' rhs=add_sub_op_expr       { ('OP', 'ADD', [lhs, rhs])}
+        | lhs=mul_div_op_expr '-' rhs=add_sub_op_expr       { ('OP', 'SUB', [lhs, rhs])}
         | mul_div_op_expr
     mul_div_op_expr:
-        | paren_expr '*' mul_div_op_expr
-        | paren_expr '/' mul_div_op_expr
+        | lhs=paren_expr '*' rhs=mul_div_op_expr            { ('OP', 'MUL', [lhs, rhs])}
+        | lhs=paren_expr '/' rhs=mul_div_op_expr            { ('OP', 'DIV', [lhs, rhs])}
         | paren_expr
     paren_expr:
-        | '(' add_sub_op_expr ')'
+        | '(' val=add_sub_op_expr ')'                       { ('OP', 'PAREN', [val])}
         | base_expr
     base_expr:
         | slice_expr
         | iden
         | value
-    slice_expr: iden ('[' iden ']')+
-    value: NUMBER
-    iden: NAME
+    slice_expr: val=iden idx=sub_op+                        { ('OP', 'SLICE', [val, *idx])}
+    sub_op: '[' iden']'                                     { iden }
+    value: NUMBER                                           { ('VALUE', int(number.string), [])}
+    iden: NAME                                              { ('IDEN', name.string, [])}
     """
 
     file = io.StringIO(source)
@@ -66,3 +71,42 @@ def parse(source):
     tokenizer = pegen.tokenizer.Tokenizer(tokengen, verbose=False)
     parser = parser_class(tokenizer, verbose=False)
     return parser.start()
+
+nodeKeys = [
+    'IDEN',
+    'VALUE',
+    'SLICE',
+    'OP',
+    'FUNC',
+    'OBJ',
+    'CONSTR',
+    'VAR',
+    'BLOCK',
+]    
+
+opKeys = [
+    'SLICE',
+    'PAREN',
+    'MUL',
+    'DIV',
+    'ADD',
+    'SUB',
+    'NE',
+    'EQ',
+    'LT',
+    'GT',
+    'LE',
+    'GE',
+    'ITER',
+]
+
+funcKeys = [
+    'FORALL',
+    'SUM',
+]
+
+varKeys = [
+    'CONT',
+    'BIN',
+    'INT'
+]
